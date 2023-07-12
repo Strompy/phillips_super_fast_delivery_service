bbafa72930d1728bb931a6ab10bc0a05ba239114
Phillip Strom <phillip.strom@gmail.com>
2023-07-12 14:18:43 -0600

    Add parse_file method

--- distance_importer.py
+++ distance_importer.py
@@ -1,8 +1,18 @@
 import csv
 
+
 class DistanceImporter:
     def __init__(self, file_path):
         self.file_path = file_path
         self.addresses = []
         self.distance_lists = []
 
+    def parse_file(self):
+        reader = csv.reader(open(self.file_path))
+        next(reader)
+        for row in reader:
+            _, address = row[0].splitlines()
+            distances = row
+            del distances[0:2]
+            self.addresses.append(address)
+            self.distance_lists.append(row)
--- tests/test_distance_importer.py
+++ tests/test_distance_importer.py
@@ -1,5 +1,6 @@
 from distance_importer import DistanceImporter
 
+
 def test_distance_importer_init():
     test_importer = DistanceImporter('../docs/distances.csv')
     assert test_importer.file_path == '../docs/distances.csv'
@@ -7,3 +8,10 @@ def test_distance_importer_init():
     assert test_importer.distance_lists == []
 
 
+def test_parse_file():
+    test_importer = DistanceImporter('../docs/distances.csv')
+    test_importer.parse_file()
+    assert len(test_importer.addresses) == 27
+    assert len(test_importer.distance_lists) == 27
+    for distances in test_importer.distance_lists:
+        assert len(distances) == 27
