7ac4be012bd8486400cb66b5b2244462c447441d
Phillip Strom <phillip.strom@gmail.com>
2023-07-12 10:05:44 -0600

    Update reader to use dict

--- package_importer.py
+++ package_importer.py
@@ -2,10 +2,11 @@ import csv
 class PackageImporter:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.packages = {}
+        self.package_arrays = []
+        self.packages = []
 
     def read_file(self):
-        with open(self.file_path) as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                print(', '.join(row))
+        reader = csv.DictReader(open(self.file_path))
+        for row in reader:
+            print(row)
+            self.package_arrays.append(row)
--- tests/test_package_importer.py
+++ tests/test_package_importer.py
@@ -2,12 +2,16 @@ import pytest
 
 from package_importer import PackageImporter
 
-
 # @pytest.fixture
 # def test_importer():
 #     return PackageImporter('./docs/packages.csv')
 
+def test_init():
+    test_importer = PackageImporter('../docs/packages.csv')
+    assert test_importer.file_path == '../docs/packages.csv'
+    assert test_importer.package_arrays == []
+
 def test_read_file():
-    test_importer = PackageImporter('./docs/packages.csv')
-    assert test_importer.file_path == './docs/packages.csv'
-    assert test_importer.packages == {}
+    test_importer = PackageImporter('../docs/packages.csv')
+    test_importer.read_file()
+    assert len(test_importer.package_arrays) == 40
