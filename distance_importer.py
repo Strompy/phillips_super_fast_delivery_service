edda6b24ad147197efdc5b537f95a32acc7eaeda
Phillip Strom <phillip.strom@gmail.com>
2023-07-12 16:01:11 -0600

    Add create_address_distances method

--- distance_importer.py
+++ distance_importer.py
@@ -6,6 +6,7 @@ class DistanceImporter:
         self.file_path = file_path
         self.addresses = []
         self.distance_lists = []
+        self.address_distances = {}
 
     def parse_file(self):
         reader = csv.reader(open(self.file_path))
@@ -16,3 +17,12 @@ class DistanceImporter:
             del distances[0:2]
             self.addresses.append(address)
             self.distance_lists.append(row)
+
+    def create_address_distances(self):
+        for index, address in enumerate(self.addresses):
+            distances = []
+            for x in range(0, index):
+                distances.append(self.distance_lists[index][x])
+            for x in range(index, 27):
+                distances.append(self.distance_lists[x][index])
+            self.address_distances[address] = distances
--- tests/test_distance_importer.py
+++ tests/test_distance_importer.py
@@ -6,6 +6,7 @@ def test_distance_importer_init():
     assert test_importer.file_path == '../docs/distances.csv'
     assert test_importer.addresses == []
     assert test_importer.distance_lists == []
+    assert test_importer.address_distances == {}
 
 
 def test_parse_file():
@@ -15,3 +16,14 @@ def test_parse_file():
     assert len(test_importer.distance_lists) == 27
     for distances in test_importer.distance_lists:
         assert len(distances) == 27
+
+def test_create_address_distances():
+    test_importer = DistanceImporter('../docs/distances.csv')
+    test_importer.parse_file()
+    test_importer.create_address_distances()
+    assert len(test_importer.address_distances) == 27
+    for distances in test_importer.address_distances.values():
+        assert len(distances) == 27
+        list(filter(('').__ne__, distances)) # removes any blank string entries from the original dataset
+        length = len(list(filter(('').__ne__, distances)))
+        assert length == 27
