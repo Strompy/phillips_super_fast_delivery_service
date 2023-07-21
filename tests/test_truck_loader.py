d6b333d43dc72719ff8326f85c5b41c9b6ed0d31
Phillip Strom <phillip.strom@gmail.com>
2023-07-21 12:13:07 -0600

    Remove route first code

--- tests/test_truck_loader.py
+++ tests/test_truck_loader.py
@@ -3,6 +3,7 @@ from package_importer import PackageImporter
 from truck import Truck
 from truck_loader import TruckLoader
 
+
 def test_truck_loader_init():
     distance_importer = DistanceImporter('../docs/distances.csv')
     distance_importer.parse_file()
@@ -16,7 +17,8 @@ def test_truck_loader_init():
     assert truck_loader.address_distances == distance_importer.address_distances
     assert truck_loader.packages == packages
     assert truck_loader.truck == truck_1
-    assert truck_loader.current_time.strftime('%H:%M:%S') == '08:00:00'
+    assert truck_loader.current_time() == '08:00:00'
+
 
 def test_load_truck():
     package_importer = PackageImporter('../docs/packages.csv')
@@ -78,85 +80,10 @@ def test_load_all_trucks():
     for package in packages:
         assert package.delivered_at_time() is not None
 
+
 def hub():
     return '1644 Platte St, Denver, CO 80202'
 
 
 def filter_packages(packages, number=None):
     return list(filter(lambda p: p.truck_number == number, packages))
-
-
-
-# First attempt tests
-
-def test_load_truck_1():
-    package_importer = PackageImporter('../docs/packages.csv')
-    package_importer.parse_file()
-    packages = package_importer.create_packages()
-    distance_importer = DistanceImporter('../docs/distances.csv')
-    distance_importer.parse_file()
-    distance_importer.create_address_distances()
-    test_truck_loader = TruckLoader(distance_importer.addresses, distance_importer.address_distances, packages)
-    route = test_truck_loader.load_truck_1()
-    assert route[0] == hub()
-    assert len(route) == 17  # this works since there are duplicate addresses
-    truck_1_packages = list(filter(lambda p: p.truck_number == 1, packages))
-    assert len(truck_1_packages) == 16
-
-
-def test_load_truck_2():
-    package_importer = PackageImporter('../docs/packages.csv')
-    package_importer.parse_file()
-    packages = package_importer.create_packages()
-    distance_importer = DistanceImporter('../docs/distances.csv')
-    distance_importer.parse_file()
-    distance_importer.create_address_distances()
-    test_truck_loader = TruckLoader(distance_importer.addresses, distance_importer.address_distances, packages)
-    route = test_truck_loader.load_truck_2()
-    assert route[0] == hub()
-    assert len(route) == 17
-    truck_2_packages = list(filter(lambda p: p.truck_number == 2, packages))
-    assert len(truck_2_packages) == 16
-
-
-def test_load_truck_3():
-    package_importer = PackageImporter('../docs/packages.csv')
-    package_importer.parse_file()
-    packages = package_importer.create_packages()
-    distance_importer = DistanceImporter('../docs/distances.csv')
-    distance_importer.parse_file()
-    distance_importer.create_address_distances()
-    test_truck_loader = TruckLoader(distance_importer.addresses, distance_importer.address_distances, packages)
-    route = test_truck_loader.load_truck_3()
-    assert route[0] == hub()
-    assert len(route) == 18
-    truck_3_packages = list(filter(lambda p: p.truck_number == 3, packages))
-    assert len(truck_3_packages) == 17
-
-
-def test_load_trucks_1_and_2_and_3():
-    package_importer = PackageImporter('../docs/packages.csv')
-    package_importer.parse_file()
-    packages = package_importer.create_packages()
-    distance_importer = DistanceImporter('../docs/distances.csv')
-    distance_importer.parse_file()
-    distance_importer.create_address_distances()
-    test_truck_loader = TruckLoader(distance_importer.addresses, distance_importer.address_distances, packages)
-    route = test_truck_loader.load_truck_1()
-    assert route[0] == hub()
-    assert len(route) == 17  # this works since there are duplicate addresses
-    truck_1_packages = list(filter(lambda p: p.truck_number == 1, packages))
-    assert len(truck_1_packages) == 16
-
-    route_2 = test_truck_loader.load_truck_2()
-    assert route_2[0] == hub()
-    assert len(route_2) == 17
-    truck_2_packages = list(filter(lambda p: p.truck_number == 2, packages))
-    assert len(truck_2_packages) == 16
-
-    route_3 = test_truck_loader.load_truck_3()
-    assert route_3[0] == hub()
-    assert len(route_3) == 9
-    truck_3_packages = list(filter(lambda p: p.truck_number == 3, packages))
-    assert len(truck_3_packages) == 8
-
